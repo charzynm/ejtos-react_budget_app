120b22c0072804003aa7f49e3e3c90ad579b5e3f
charzynm <charzynm@wp.pl>
2024-05-31 10:05:01 +0000

    added new feature

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,11 +4,24 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
     const handleBudgetChange = (event) => {
+        if(newBudget >= 20000) {
+            alert("The value cannot exceed 20000");
+            setNewBudget(20000);
+            return;
+        }
+        if(newBudget < expenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+            setNewBudget(expenses);
+            return;
+        }
+
         setNewBudget(event.target.value);
     }
+    const alertType = newBudget > 20000 ? 'alert-danger' : 'alert-success';
     return (
-<div className='alert alert-secondary'>
+<div className={`alert ${alertType}`}>
 <span>Budget: £{budget}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
