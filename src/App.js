917437e3334cbf4dff19f6452dbce32643a919c5
charzynm <charzynm@wp.pl>
2024-05-31 08:42:21 +0000

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
